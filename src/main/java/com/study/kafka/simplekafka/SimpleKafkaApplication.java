685ccd621fef6beae4738a1ac112608af0351bf0
김경륜 <kyoungryun.kim@innowireless.com>
2024-01-04 16:33:04 +0900

    Init: .

---
+++ src/main/java/com/study/kafka/simplekafka/SimpleKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.study.kafka.simplekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleKafkaApplication.class, args);
+	}
+
+}
